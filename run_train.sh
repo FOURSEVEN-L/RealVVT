4d1fff9e7fd3ba2e522d5d1a5498a6eaee71ba84
FOURSEVEN-L <68238659+FOURSEVEN-L@users.noreply.github.com>
2025-03-30 23:51:01 +0800

    Update and rename run_train_pose_animation.sh to run_train.sh

---
+++ run_train.sh
@@ -0,0 +1,2 @@
+accelerate launch --config_file configs/ds/zero2_config.yaml train.py \
+    --config configs/train/videovvt.yaml
--- run_train_pose_animation.sh
+++
@@ -1,2 +0,0 @@
-accelerate launch --config_file configs/ds/zero2_config.yaml train_pose_animation_vivid.py \
-    --config configs/train/pose_animation.yaml
